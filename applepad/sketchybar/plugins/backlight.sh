78bf37dbb2a0f155bf2435f95845bb7d7bfdf1ef
lily <lily@lilysthings.org>
2024-04-27 18:06:52 -0400

    changes to sketchybar config

--- applepad/sketchybar/plugins/backlight.sh
+++ applepad/sketchybar/plugins/backlight.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 
-# The $NAME variable is passed from sketchybar and holds the name of
-# the item invoking this script:
-# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+# The brightness_change event supplies a $INFO variable in which the current backlight brightness
+# percentage is passed to the script.
 
-sketchybar --set "$NAME" label="$(echo "$(brightness -l | awk 'NR==2 {print $4}') * 100" | bc -l | awk '{printf "%.f\n", int($1+0.5)}')%"
+if [ "$SENDER" = "brightness_change" ]; then
+	BRIGHTNESS="$INFO"
 
+	case "$BRIGHTNESS" in
+		[9][0-9]|100) ICON=""
+		;;
+		[8][0-9]) ICON=""
+		;;
+		[7][0-9]) ICON=""
+		;;
+		[6][0-9]) ICON=""
+		;;
+		[5][0-9]) ICON=""
+		;;
+		[4][0-9]) ICON=""
+		;;
+		[3][0-9]) ICON=""
+		;;
+		[2][0-9]) ICON=""
+		;;
+		[1][0-9]) ICON=""
+		;;
+		*) ICON=""
+	esac
+
+	sketchybar --set "$NAME" icon="$ICON" label="$BRIGHTNESS%"
+fi
--- applepad/sketchybar/plugins/clock.sh
+++ applepad/sketchybar/plugins/clock.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # The $NAME variable is passed from sketchybar and holds the name of
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date +'%l:%M %p')"
+if [ $(date +%l) >= 10 ]; then
+	sketchybar --set "$NAME" label.padding_right=10
+else
+	sketchybar --set "$NAME" label.padding_right=20
+fi
 
+sketchybar --set "$NAME" label="$(date +'%l:%M %p')"
